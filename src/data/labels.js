9f6c3c40ab484e0bd5f92a83fbf670767ec0e00c
daniel allott <danielallott@aol.com>
2024-05-28 19:45:08 +0100

    more labels

--- src/data/labels.js
+++ src/data/labels.js
@@ -1,23 +1,44 @@
 export const labels = [
-      {
-          "name":"SOLO TERRA",
-          "size": 3,
-          "latitude":15,
-          "longitude":-20,
-          "color":"#FF000044"
-      },
-      {
-        "name":"BOREA TERRA",
-        "size": 3,
-        "latitude":30,
-        "longitude":80,
-        "color":"#FF000044"
+    {
+      "name":"SOLO TERRA",
+      "size": 3,
+      "latitude":15,
+      "longitude":-20,
+      "color":"#FF000044"
+    },
+    {
+      "name":"BOREA TERRA",
+      "size": 3,
+      "latitude":30,
+      "longitude":80,
+      "color":"#FF000044"
     },
     {
       "name":"MERIDIO TERRA",
       "size": 3,
-      "latitude":-13,
-      "longitude":125,
+      "latitude":-25,
+      "longitude":127,
+      "color":"#FF000044"
+    },
+    {
+      "name":"ABLE TERRA",
+      "size": 1,
+      "latitude":-60,
+      "longitude":30,
+      "color":"#FF000044"
+    },
+    {
+      "name":"COIL TERRA",
+      "size": 1,
+      "latitude":5,
+      "longitude":110,
+      "color":"#FF000044"
+    },
+    {
+      "name":"TWIN TERRA",
+      "size": 1,
+      "latitude":-40,
+      "longitude":-114,
       "color":"#FF000044"
     },
     {
@@ -25,6 +46,13 @@ export const labels = [
       "size": 1,
       "latitude":41,
       "longitude":-55,
+      "color":"#FF000044"
+    },
+    {
+      "name":"Thousand Isles",
+      "size": 1,
+      "latitude":20,
+      "longitude":-85,
       "color":"#FF000044"
     },
     {
@@ -40,5 +68,96 @@ export const labels = [
       "latitude":62,
       "longitude": -32,
       "color":"#FF000044"
+    },
+    {
+      "name":"Mount Indigo",
+      "size": 1,
+      "latitude":2,
+      "longitude":-48,
+      "color":"#FF000044"
+    },
+    {
+      "name":"Comet Range",
+      "size": 1,
+      "latitude":-18,
+      "longitude":-15,
+      "color":"#FF000044"
+    },
+    {
+      "name":"Echo Massif",
+      "size": 1,
+      "latitude":-4,
+      "longitude":20,
+      "color":"#FF000044"
+    },
+    {
+      "name":"Noble Mountains",
+      "size": 1,
+      "latitude":8,
+      "longitude":135,
+      "color":"#FF000044"
+    },
+    {
+      "name":"Zenith Orogeny",
+      "size": 1,
+      "latitude":-50,
+      "longitude":125,
+      "color":"#FF000044"
+    },
+    {
+      "name":"Fortune Range",
+      "size": 1,
+      "latitude":-42,
+      "longitude":110,
+      "color":"#FF000044"
+    },
+    {
+      "name":"Harvest Range",
+      "size": 1,
+      "latitude":-14,
+      "longitude":97,
+      "color":"#FF000044"
+    },
+    {
+      "name":"Middle Sea Floodlands",
+      "size": 1,
+      "latitude":-16,
+      "longitude":115,
+      "color":"#FF000044"
+    },
+    {
+      "name":"BOREA STRAIT",
+      "size": 2,
+      "latitude":70,
+      "longitude":80,
+      "color":"#0000FF44"
+    },
+    {
+      "name":"LAGUNAN STRAIT",
+      "size": 2,
+      "latitude":24,
+      "longitude":29,
+      "color":"#0000FF44"
+    },
+    {
+      "name":"MERIDO OCEAN",
+      "size": 2,
+      "latitude":-30,
+      "longitude":29,
+      "color":"#0000FF44"
+    },
+    {
+      "name":"MIDDLE SEA",
+      "size": 2,
+      "latitude":10,
+      "longitude":89,
+      "color":"#0000FF44"
+    },
+    {
+      "name":"NOBLE OCEAN",
+      "size": 2,
+      "latitude":20,
+      "longitude":-150,
+      "color":"#0000FF44"
     },
   ]
