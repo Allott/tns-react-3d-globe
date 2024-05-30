2902e513979a5406ea23f57092f2d4d85c034147
daniel allott <danielallott@aol.com>
2024-05-30 22:16:08 +0100

    styling

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -7,7 +7,7 @@ return (
             <motion.div 
                 className={`fixed sm:rounded-tl-lg bg-gray-800 text-white z-40 bottom-0 right-0 w-full sm:w-1/3 h-1/3 lg:h-1/2`}
                 initial={{ y: "calc(100vh + 50%)" }}
-                animate={{ y: 0}}
+                animate={{ y: -10}}
                 exit={{ y: "calc(100vh + 50%)" }}
                 transition={{ bounce : 0, duration: 0.8 }}
             >
@@ -15,7 +15,7 @@ return (
                     <h1 className="mx-2">{'>> ' + name}</h1>
                     <button className='p-2 px-4 hover:text-red-600' onClick={() => setDisplayData(null)}>X</button>
                 </div>
-                <div className="overflow-scroll m-4 h-full">
+                <div className="overflow-auto m-4 h-4/5">
                     {content?.map((item, index) => (
                         <p key={index} className={item?.class ? item?.class : 'text-white pb-2'}>
                             {item?.text}
