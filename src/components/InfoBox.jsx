53c80a5754eb6f450598d549b69ac8d7f0c106c6
daniel allott <danielallott@aol.com>
2024-05-30 22:18:14 +0100

    typo

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -7,7 +7,7 @@ return (
             <motion.div 
                 className={`fixed sm:rounded-tl-lg bg-gray-800 text-white z-40 bottom-0 right-0 w-full sm:w-1/3 h-1/3 lg:h-1/2`}
                 initial={{ y: "calc(100vh + 50%)" }}
-                animate={{ y: -10}}
+                animate={{ y: 0}}
                 exit={{ y: "calc(100vh + 50%)" }}
                 transition={{ bounce : 0, duration: 0.8 }}
             >
